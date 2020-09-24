040c0f05f78f80e05bf1abc6f9704bbad398cffa
Robert Laws <roblaws@me.com>
2020-09-24 13:44:03 +0300

    remove semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.scss';
 import './firebase/config';
-import { Container } from 'semantic-ui-react';
-import { Signup } from './pages';
+import { Header } from './layout';
+import { Home, Signup } from './pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Container>
+      <div>
+        <Header />
         <Switch>
-          <Route exact path='/signup'>
-            <Signup />
-          </Route>
+          <Route exact path='/' component={Home} />
+          <Route path='/signup' component={Signup} />
         </Switch>
-      </Container>
+      </div>
     </Router>
   );
 }
